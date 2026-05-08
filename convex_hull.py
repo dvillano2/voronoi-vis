ae65c34c6907a9b191bd572216748796c75b6448
Max Darling <maxdarling11@gmail.com>
2026-05-08 12:06:27 -0400

    basic plot

--- convex_hull.py
+++ convex_hull.py
@@ -1,5 +1,5 @@
+import matplotlib.pyplot as plt
 from math import sqrt
-
 
 def find_ref_point(points):
     return min(points, key=lambda p: (p[1], p[0]))
@@ -38,22 +38,29 @@ def scan(ordered_points):
         while len(stack) > 1 and not left_turn(stack[-2], stack[-1], point):
             stack.pop()
         stack.append(point)
-    return stack[:-1]
+    # return stack[:-1]
+    return stack
 
-
-
-def test(points, expected):
+def plot_voronoi(points):
     ref_point = find_ref_point(points)
     ordered = order_points(points)
     scanned = scan(ordered)
+    fig, ax = plt.subplots()
+    ax.plot([p[0] for p in scanned], [p[1] for p in scanned])
+    plt.show()
 
-    print(f'points: {points}')
-    print(f'ref_point: {ref_point}')
-    print(f'ordered: {ordered}')
-    print(f'scanned: {scanned}')
-    print(f'expected: {expected}')
-    print("PASSED" if expected == scanned else "FAILED")
-    print("\n")
+# def test(points, expected):
+#     ref_point = find_ref_point(points)
+#     ordered = order_points(points)
+#     scanned = scan(ordered)
+
+#     print(f'points: {points}')
+#     print(f'ref_point: {ref_point}')
+#     print(f'ordered: {ordered}')
+#     print(f'scanned: {scanned}')
+#     print(f'expected: {expected}')
+#     print("PASSED" if expected == scanned else "FAILED")
+#     print("\n")
 
 
 # case 1
@@ -65,3 +72,7 @@ expected = [[-2, -1], [4, 3], [9, 7], [2, 7]]
 points = [[4, 5], [0, 0], [9, 7], [2, 7]]
 expected = [[0, 0], [9, 7], [2, 7]]
 # test(points, expected)
+
+
+# PLOTTING
+plot_voronoi(points)
