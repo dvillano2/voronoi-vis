1890a112d45748cdb852bc0a8ae2a67808882036
dvillano2 <domvillano@gmail.com>
2026-05-20 11:46:23 -0400

    formatting

--- history_dag.py
+++ history_dag.py
@@ -99,9 +99,7 @@ class HistoryDAG:
                 continue
             t, r = self.edge_to_tris[opp_edge]
             if circle_test(t, r, [opp_edge.points[0], opp_edge.points[1]]):
-                new_nodes = self.flip(
-                    t, r, [opp_edge.points[0], opp_edge.points[1]]
-                )
+                new_nodes = self.flip(t, r, [opp_edge.points[0], opp_edge.points[1]])
                 stack += new_nodes
                 # print("STACK", stack)
                 print("flipped")
@@ -130,12 +128,8 @@ class HistoryDAG:
                     self.edge_to_tris[local_edge] = [node, to_keep[0]]
         del self.edge_to_tris[edge]
 
-        print(
-            f"flip - input triangles: {t.triangle.points}{r.triangle.points}"
-        )
-        print(
-            f"flip - output triangles: {[t.triangle.points for t in new_nodes]}"
-        )
+        print(f"flip - input triangles: {t.triangle.points}{r.triangle.points}")
+        print(f"flip - output triangles: {[t.triangle.points for t in new_nodes]}")
         return new_nodes
 
     def get_leaves(self):
