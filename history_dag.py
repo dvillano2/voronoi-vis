a2ee25eae7a85db8c3935132edf045cfd2a5b551
dvillano2 <domvillano@gmail.com>
2026-05-26 18:32:48 -0400

    dummy changes

--- history_dag.py
+++ history_dag.py
@@ -172,7 +172,14 @@ def circle_test(t: TriangleNode, r: TriangleNode, e: Edge):
     if not e.p.is_finite and not e.q.is_finite:
         return False
     # should not flip to edge at infinity
-    if not present_point.is_finite and not last_point.q.is_finite:
+    if not present_point.is_finite and not last_point.is_finite:
+        return False
+
+    # DUMMY FOR TESTS
+    if not e.p.is_finite or not e.q.is_finite:
+        return False
+    # should not flip to edge at infinity
+    if not present_point.is_finite or not last_point.is_finite:
         return False
 
     # don't flip finite edge to infinte edge
@@ -188,31 +195,31 @@ def circle_test(t: TriangleNode, r: TriangleNode, e: Edge):
     # if infinite edge to be flipped crossed with
     # finite edge that replaces it, do it,
     # otherwise don't
-    if not e.p.is_finite or not e.q.is_finite:
-        # refactor
+    # if not e.p.is_finite or not e.q.is_finite:
+    # refactor
 
-        # points lie on the same side of the line
-        # so
-        if Point.is_left(e.p, e.q, present_point) == Point.is_left(
-            e.p, e.q, last_point
-        ):
-            return False
+    # points lie on the same side of the line
+    # so
+    # if Point.is_left(e.p, e.q, present_point) == Point.is_left(
+    #     e.p, e.q, last_point
+    # ):
+    #     return False
 
-        # tripled_t = t.triple()
-        # tripled_r = r.triple()
-        # a, b = present_point.x, present_point.y
-        # z, w = last_point.x, last_point.y
-        # interpolated_point_1 = Point(a + z, 2 * b + 2 * w)
-        # interpolated_point_2 = Point(2 * a + 2 * z, b + w)
-        # condition_1 = tripled_t.contains(
-        #     interpolated_point_1
-        # ) or tripled_r.contains(interpolated_point_2)
-        # condition_2 = tripled_t.contains(
-        #     interpolated_point_2
-        # ) or tripled_r.contains(interpolated_point_2)
-        # if condition_1 or condition_2:
-        #     return False
-        # return True
+    # tripled_t = t.triple()
+    # tripled_r = r.triple()
+    # a, b = present_point.x, present_point.y
+    # z, w = last_point.x, last_point.y
+    # interpolated_point_1 = Point(a + z, 2 * b + 2 * w)
+    # interpolated_point_2 = Point(2 * a + 2 * z, b + w)
+    # condition_1 = tripled_t.contains(
+    #     interpolated_point_1
+    # ) or tripled_r.contains(interpolated_point_2)
+    # condition_2 = tripled_t.contains(
+    #     interpolated_point_2
+    # ) or tripled_r.contains(interpolated_point_2)
+    # if condition_1 or condition_2:
+    #     return False
+    # return True
 
     # if everything is finite, go back to circle test
     def pull_coords(p: Point):
